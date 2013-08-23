4ceca6e496e50f596f3adc14be15b3668addf16a
Thorsten Kahler <thorsten.kahler@typo3.org>
2013-08-23 13:03:37 +0200

    [FEATURE] Kickstart session API
    add new classes to TYPO3\CMS\Core:
    * Session\Data is an OO wrapper for session info
    * Session\StorageInterface defines API for session storage
      implementations
    
    Change-Id: I800b9c6168def273ee864d877635cfed071f3b65

---
+++ typo3/sysext/core/Classes/Session/Data.php
@@ -0,0 +1,98 @@
+<?php
+namespace TYPO3\CMS\Core\Session;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Thorsten Kahler <thorsten.kahler@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * Data structure for session data
+ *
+ * @package TYPO3\CMS\Core\Session
+ */
+class Data {
+
+	/**
+	 * @var array $content the payload
+	 */
+	protected $content = array();
+
+	/**
+	 * @var integer $tstamp last storage time (Unix epoche)
+	 */
+	protected $tstamp = 0;
+
+	/**
+	 * @var string $identifier
+	 */
+	protected $identifier;
+
+	/**
+	 * @param array $content
+	 */
+	public function setContent($content)
+	{
+		$this->content = $content;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	/**
+	 * @param string $identifier
+	 */
+	public function setIdentifier($identifier)
+	{
+		$this->identifier = $identifier;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getIdentifier()
+	{
+		return $this->identifier;
+	}
+
+	/**
+	 * @param int $tstamp
+	 */
+	public function setTstamp($tstamp)
+	{
+		$this->tstamp = $tstamp;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getTstamp()
+	{
+		return $this->tstamp;
+	}
+
+}
---
+++ typo3/sysext/core/Classes/Session/StorageInterface.php
@@ -0,0 +1,68 @@
+<?php
+namespace TYPO3\CMS\Core\Session;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Thorsten Kahler <thorsten.kahler@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Session Storage
+ *
+ *
+ *
+ * @package TYPO3\CMS\Core\Session
+ */
+interface StorageInterface {
+
+	/**
+	 * Fetch session data
+	 *
+	 * @param string $identifier the session ID
+	 * @return Data session data object
+	 */
+	public function get($identifier);
+
+	/**
+	 * Store session data
+	 *
+	 * @param Data $sessionData
+	 * @return boolean TRUE on success
+	 */
+	public function put(Data $sessionData);
+
+	/**
+	 * Delete session data
+	 *
+	 * @param string $identifier the session ID
+	 * @return boolean TRUE on success
+	 */
+	public function delete($identifier);
+
+	/**
+	 * Garbage collection removes outdated session data
+	 *
+	 * @return void
+	 */
+	public function collectGarbage();
+
+
+}
